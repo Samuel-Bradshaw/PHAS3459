6b5d392c16c9af44f4bce13d81efc0845c13b7ce
zcapscb <zcapscb@CT00019442.ad.ucl.ac.uk>
2016-10-05 12:50:09 +0100

    Starting to build void methods outside of main loop instead of inside main loop

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -1,12 +1,24 @@
 package module1;
 
 public class AlgorithmControl {
+	
+	void loop (int i, int j) {
+		for (System.out.println(i) ; i <= j; i = i + 1);
+	}
 
 	public static void main(String[] args) {
+		
+		//Using a for loop to print integers from 1 to 20
 		int i;
 		for (i = 1; i < 21; i = i + 1)
 		{System.out.println(i);}
 		
+		//Using a while loop to print integers between 8 to -10
+		int j = 8;
+		while (j > -11) {
+			System.out.println(j);
+			j = j-1;
+		}
 
 	}
 

a4d4801659cf54703034fb8d789aeb0f9ed7cfc3
zcapscb <zcapscb@CT00019442.ad.ucl.ac.uk>
2016-10-05 12:09:45 +0100

    Completed magnitude and dot product function. Angle function completed

--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -1,13 +1,40 @@
 package module1;
 
-public class VectorMethods {
+import java.lang.Math;
 
+public class VectorMethods {
+	
+	//defining dot product function 
+	 double dotProduct (double x1, double y1, double z1,
+	 			double x2, double y2, double z2) {
+
+		 double x = x1*x2;
+		 double y = y1*y2;
+		 double z = z1*z2;
+
+		 return x + y + z; 
+	 	}
+
+	 
+	//magnitude function
+	 double magnitude (double x, double y, double z) {
+		  
+		 
+		 return Math.sqrt((x*x)+(y*y)+(z*z));
+				 
+		 
+	 }
+	 
 	public static void main(String[] args) {
 		
 		VectorMethods vm = new VectorMethods();
 		
-		double mag = vm.magnitude(x,y,z);
+		double dp1 = vm.dotProduct(1 ,2, 3, 4, 5, 6);
+		System.out.println(dp1);
 		
+		
+		double mag1 = vm.magnitude(1,2,2);
+		System.out.println(mag1);
 		
 		
 	
@@ -17,13 +44,5 @@ public class VectorMethods {
 
 }
  
- double dotProduct ((double x1, double y1, double z1),
-		 			(double x2, double y2, double z2) {
-	 
-	 double x = x1*x2;
-	 double y = y1*y2;
-	 double z = z1*z2;
-	 
-	 retur
-	 
- }
+
+ 

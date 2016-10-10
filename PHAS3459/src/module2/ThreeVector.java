279ff6b8f6e51ed8bfffee4d98924843ca434306
zcapscb <zcapscb@CT00006950.ad.ucl.ac.uk>
2016-10-10 15:05:39 +0100

    About to delete "main" class line of coed -mistakenly made a "main" class instead of a standard class

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -1,9 +1,21 @@
 package module2;
 
-public class ThreeVector {
+import java.lang.Math;
 
+
+public class ThreeVector {
+	
+	double magnitude(double x, double y, double z){
+		 return Math.sqrt((x*x)+(y*y)+(z*z));}
+	
+	ThreeVector unitVector(double x, double y, double z){ //do I define the method here 
+		return (x, y, z)/Math.sqrt((x*x)+(y*y)+(z*z));    // or inside the main part? 
+	} // how do I store a vector? 
+ 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ThreeVector tv = new ThreeVector();
+		
 
 	}
 

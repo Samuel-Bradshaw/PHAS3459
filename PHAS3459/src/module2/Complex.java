eed5d0ea2703e50eb59f92a0d10214e0fd911ea1
zcapscb <zcapscb@CT00019342.ad.ucl.ac.uk>
2016-10-12 12:40:24 +0100

    Complex class start

---
+++ PHAS3459/src/module2/Complex.java
@@ -0,0 +1,27 @@
+package module2;
+
+public class Complex {
+	
+	//Member variables: 
+	//x corresponds to the real component, 
+	//y corresponds to the imaginary component:
+	double x; double y;
+	
+	//Constructor
+	public Complex(double real, double imaginary){
+		x = real;
+		y = imaginary;
+	}
+
+	double real(){
+		return x;
+	}
+	
+	double imag(){
+		return y;
+	}
+	
+	double modulus(){
+		//tan(theta)
+	}
+}
---
+++ PHAS3459/src/module2/TestComplex.java
@@ -0,0 +1,10 @@
+package module2;
+
+public class TestComplex {
+
+	public static void main(String[] args) {
+		Complex z = new Complex(1,1); 
+
+	}
+
+}

1ddbbcf0ec4d84e0458ca74b2d2ae346a57a3724
zcapscb <zcapscb@CT00006967.ad.ucl.ac.uk>
2016-10-24 14:11:54 +0100

    Word counter has small problem with scanner but otherwise working alright. Made start on Numerical reader

---
+++ PHAS3459/src/module4/NumericalReader.java
@@ -0,0 +1,84 @@
+package module4; 
+
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*; 
+
+public class NumericalReader { 
+
+	public static String getStringFromKeyboard() throws IOException{ 
+		System.out.println("Please enter a string:"); 
+		InputStreamReader input1 = new InputStreamReader(System.in); 
+		BufferedReader br = new BufferedReader(input1); 
+		String string1 = br.readLine(); 
+		return string1; 
+	} 
+
+	public BufferedReader brFromURL(String urlName)throws IOException{ 
+		URL url = new URL(urlName); 
+		InputStream istream = url.openStream(); 
+		InputStreamReader isr = new InputStreamReader(istream); 
+		BufferedReader buffr = new BufferedReader(isr); 
+		return buffr; 
+	} 
+
+	void analysisStart(String dataFile) throws IOException{ 
+		//NEXT LINE NEEDS CHANGING TO WHERE WE WANT THE FILE TO BE STORED 
+		String datafile1 = ("N:" + File.separator + "mywork" + File.separator + dataFile); 
+		File outputfile = new File(datafile1); 
+		FileWriter fw = new FileWriter(outputfile); 
+		//Initialising values -NEEDS T0 BE PRIVATE? 
+		double minValue; 
+		double maxValue; 
+		double nValues; 
+		double sumOfValues;  
+	} 
+
+	void analyseData(String line){ 
+		//Ignore blank and comment lines 
+		char firstCharacterOfLine = line.trim().charAt(0);//isolating the first character of string to see if line is a comment 
+		if( line.trim().isEmpty() || Character.isLetter(firstCharacterOfLine) ){ 
+		}  
+		//For lines containing numbers: write these numbers  
+		//one per line to the screen AND to a file; 
+		else{ 
+			Scanner sc1 = new Scanner(line);  
+			while(sc1.hasNext()){ 
+				String token = sc1.next(); 
+				System.out.println(token); 
+				//WRITE THESE NUMBERS TO A FILE 
+				//using number read in, update minValue, maxValue, nValues, sumofValues 
+
+			} 
+		} 
+	} 
+	// Prints  the minimum value, the maximum value, the average value and the total number of values read. 
+	void analysisEnd(){ 
+		System.out.println("minValue: "+minValue); 
+		System.out.println("maxValue: "+maxValue); 
+		System.out.println("nValues: "+nValues); 
+		System.out.println("sumOfValues: "+sumOfValues); 
+	} 
+
+	public static void main(String[] args) throws IOException { 
+		NumericalReader nm = new NumericalReader(); 
+
+
+		//	String a = getStringFromKeyboard(); 
+		//	char b = a.charAt(0); 
+		//	if(Character.isLetter(b)){ 
+		//		System.out.println(b); 
+		//	} 
+		String st1 = "198 0 97"; 
+		System.out.println(st1);  
+
+		Scanner sc1 = new Scanner(st1);  
+		while(sc1.hasNext()){ 
+			String token = sc1.next(); 
+			System.out.println(token); 
+		} 
+	} 
+
+
+} 
---
+++ PHAS3459/src/module4/WordCounter.java
@@ -0,0 +1,48 @@
+package module4; 
+
+//Counts the number of words in an online document 
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*; 
+
+public class WordCounter { 
+
+	public static BufferedReader brFromURL(String urlName) throws IOException{ 
+		URL url = new URL(urlName); 
+		InputStream istream = url.openStream(); 
+		InputStreamReader isr = new InputStreamReader(istream); 
+		BufferedReader buffr = new BufferedReader(isr); 
+		return buffr;} 
+
+
+	public static BufferedReader brFromFile(String fileName) throws IOException{ 
+		FileReader fr = new FileReader("filename"); 
+		BufferedReader br = new BufferedReader(fr); 
+		return br; 
+	} 
+
+	public static int countWordsInResource(BufferedReader dataAsBR){ 
+		try(Scanner scanner1 = new Scanner(dataAsBR); 
+				){ 
+			int wordcount = 0; //initialising word count 
+			while (scanner1.hasNext()){ //As long as there is a word after the current position in document... 
+				wordcount++; //...increment the word count...  
+				scanner1.next(); //...and shift position to next word in document 
+			} 
+			return wordcount; 
+		} 
+	} 
+
+	public static void main(String[] args) { 
+		try { 
+			BufferedReader br = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"); 
+			int count = countWordsInResource(br); 
+			System.out.println("Wordcount:"+count); 
+		}  
+		catch (IOException e) { 
+			e.printStackTrace(); 
+		}  
+	} 
+
+} 

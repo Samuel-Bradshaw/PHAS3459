c0ddee5e9b1e7a1c9fc580cccf994afd9580fc2a
zcapscb <zcapscb@CT00006967.ad.ucl.ac.uk>
2016-10-24 16:08:32 +0100

    Working on analyseData - writing the variables to a file

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -1,11 +1,20 @@
 package module4; 
 
-
 import java.io.*; 
 import java.net.*; 
 import java.util.*; 
-
+/** 
+ * 
+ * @author zcapscb
+ *
+ */
 public class NumericalReader { 
+	
+	//Member variables
+	private double minValue; 
+	private double maxValue; 
+	private double nValues; 
+	private double sumOfValues;
 
 	public static String getStringFromKeyboard() throws IOException{ 
 		System.out.println("Please enter a string:"); 
@@ -24,61 +33,66 @@ public class NumericalReader {
 	} 
 
 	void analysisStart(String dataFile) throws IOException{ 
-		//NEXT LINE NEEDS CHANGING TO WHERE WE WANT THE FILE TO BE STORED 
-		String datafile1 = ("N:" + File.separator + "mywork" + File.separator + dataFile); 
-		File outputfile = new File(datafile1); 
-		FileWriter fw = new FileWriter(outputfile); 
-		//Initialising values -NEEDS T0 BE PRIVATE? 
-		double minValue; 
-		double maxValue; 
-		double nValues; 
-		double sumOfValues;  
+		//datafile represents the Directory where we want the file to be stored and the file name
+		File outputfile = new File(dataFile); 
+		outputfile.mkdirs();
+		outputfile.createNewFile();
+		System.out.println("File "+dataFile+" created.");
+		//FileWriter fw = new FileWriter(outputfile);
+		//Initialising values 
+		minValue = 0.0; 
+		maxValue = 0.0; 
+		nValues = 0.0; 
+		sumOfValues = 0.0;  
 	} 
 
-	void analyseData(String line){ 
-		//Ignore blank and comment lines 
+	void analyseData(String line) throws IOException { 
+		
 		char firstCharacterOfLine = line.trim().charAt(0);//isolating the first character of string to see if line is a comment 
-		if( line.trim().isEmpty() || Character.isLetter(firstCharacterOfLine) ){ 
+		if( line.trim().isEmpty() || Character.isLetter(firstCharacterOfLine) ){ //-ignoring blank and comment lines.
 		}  
 		//For lines containing numbers: write these numbers  
 		//one per line to the screen AND to a file; 
 		else{ 
-			Scanner sc1 = new Scanner(line);  
+			try(Scanner sc1 = new Scanner(line);
+					){
+				 //try creates error
+					while(sc1.hasNext()){ 
+						String token = sc1.next(); 
+						System.out.println(token); 
+						//WRITE THESE NUMBERS TO A FILE 
+						//using number read in, update minValue, maxValue, nValues, sumofValues 
+
+					}
+				} 
+			} 
+		} 
+		// Prints  the minimum value, the maximum value, the average value and the total number of values read. 
+		void analysisEnd(){ 
+			System.out.println("minValue: "+minValue); 
+			System.out.println("maxValue: "+maxValue); 
+			System.out.println("nValues: "+nValues); 
+			System.out.println("sumOfValues: "+sumOfValues); 
+		} 
+
+		public static void main(String[] args) throws IOException { 
+			NumericalReader nm = new NumericalReader(); 
+
+
+			//	String a = getStringFromKeyboard(); 
+			//	char b = a.charAt(0); 
+			//	if(Character.isLetter(b)){ 
+			//		System.out.println(b); 
+			//	} 
+			String st1 = "198 0 97"; 
+			System.out.println(st1);  
+
+			Scanner sc1 = new Scanner(st1);  
 			while(sc1.hasNext()){ 
 				String token = sc1.next(); 
 				System.out.println(token); 
-				//WRITE THESE NUMBERS TO A FILE 
-				//using number read in, update minValue, maxValue, nValues, sumofValues 
-
 			} 
 		} 
+
+
 	} 
-	// Prints  the minimum value, the maximum value, the average value and the total number of values read. 
-	void analysisEnd(){ 
-		System.out.println("minValue: "+minValue); 
-		System.out.println("maxValue: "+maxValue); 
-		System.out.println("nValues: "+nValues); 
-		System.out.println("sumOfValues: "+sumOfValues); 
-	} 
-
-	public static void main(String[] args) throws IOException { 
-		NumericalReader nm = new NumericalReader(); 
-
-
-		//	String a = getStringFromKeyboard(); 
-		//	char b = a.charAt(0); 
-		//	if(Character.isLetter(b)){ 
-		//		System.out.println(b); 
-		//	} 
-		String st1 = "198 0 97"; 
-		System.out.println(st1);  
-
-		Scanner sc1 = new Scanner(st1);  
-		while(sc1.hasNext()){ 
-			String token = sc1.next(); 
-			System.out.println(token); 
-		} 
-	} 
-
-
-} 
--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -7,7 +7,7 @@ import java.net.*;
 import java.util.*; 
 
 public class WordCounter { 
-
+	//Returns a buffered reader of an online file.
 	public static BufferedReader brFromURL(String urlName) throws IOException{ 
 		URL url = new URL(urlName); 
 		InputStream istream = url.openStream(); 
@@ -15,24 +15,26 @@ public class WordCounter {
 		BufferedReader buffr = new BufferedReader(isr); 
 		return buffr;} 
 
-
+	//Returns a buffered reader of a local file. 
 	public static BufferedReader brFromFile(String fileName) throws IOException{ 
 		FileReader fr = new FileReader("filename"); 
 		BufferedReader br = new BufferedReader(fr); 
 		return br; 
 	} 
 
-	public static int countWordsInResource(BufferedReader dataAsBR){ 
-		try(Scanner scanner1 = new Scanner(dataAsBR); 
-				){ 
-			int wordcount = 0; //initialising word count 
+	public static int countWordsInResource(BufferedReader dataAsBR) throws IOException { 
+		try(Scanner scanner1 = new Scanner(dataAsBR);
+				){  
+			int wordcount = 0; 			//initialising word count 
 			while (scanner1.hasNext()){ //As long as there is a word after the current position in document... 
-				wordcount++; //...increment the word count...  
-				scanner1.next(); //...and shift position to next word in document 
+				wordcount++; 			//...increment the word count...  
+				scanner1.next(); 		//...and shift position to next word in document 
 			} 
-			return wordcount; 
-		} 
+			
+			return wordcount;
+		}
 	} 
+
 
 	public static void main(String[] args) { 
 		try { 
@@ -43,6 +45,7 @@ public class WordCounter {
 		catch (IOException e) { 
 			e.printStackTrace(); 
 		}  
+
 	} 
 
 } 

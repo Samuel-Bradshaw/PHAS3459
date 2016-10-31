676203262b30bb03d5c5179bd51ff742dcab4c78
zcapscb <zcapscb@CT00006950.ad.ucl.ac.uk>
2016-10-31 14:45:27 +0000

    module4 complete

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -1,22 +1,25 @@
 package module4; 
 
+/** 
+ * Reads data from an online document, 
+ * prints it to the screen and writes it to a .txt document. 
+ * @author zcapscb
+ */
+
 import java.io.*; 
 import java.net.*; 
-import java.util.*; 
-/** 
- * 
- * @author zcapscb
- *
- */
+import java.util.*;
+
 public class NumericalReader { 
 
 	//Member variables
 	private double minValue; 
 	private double maxValue; 
-	private double nValues; 
+	private int nValues; 
 	private double sumOfValues;
 	private String dataFile1;
-	private static String filename = "numbers.txt";
+	private static String filename1 = "numbers1.txt";
+	private static String filename2 = "numbers2.txt";
 
 	//Asks user to enter a string and stores the entered string. 
 	public static String getStringFromKeyboard() throws IOException{ 
@@ -27,7 +30,7 @@ public class NumericalReader {
 		return string1; 
 	} 
 
-	//Reads an online document and returns the resulting 
+	//Reads an online document and returns the resulting BufferedReader
 	public BufferedReader brFromURL(String urlName) throws IOException{ 
 		URL url = new URL(urlName); 
 		InputStream istream = url.openStream(); 
@@ -36,11 +39,11 @@ public class NumericalReader {
 		return buffr; 
 	} 
 
-	//Creates a data file 
+	//Creates a data file  and initialises values for minValue, MaxValue, nValues, sumOfValues
 	void analysisStart(String dataFile) throws IOException{ 
 		//datafile represents the Directory where we want the file to be stored and the file name
 		File outputfile = new File(dataFile); 
-		outputfile.mkdirs();
+		outputfile.getParentFile().mkdirs();
 		outputfile.createNewFile();
 		System.out.println("File "+dataFile+" created.");
 
@@ -48,39 +51,42 @@ public class NumericalReader {
 		dataFile1 = dataFile;
 
 		//Initialising values 
-		minValue = 0.0; 
+		minValue = 100000.0; //this needs to be large so that the actual minValue in the file is smaller than the initialised value 
 		maxValue = 0.0; 
-		nValues = 0.0; 
-		sumOfValues = 0.0;  
+		nValues = 0; 
+		sumOfValues = 0.0; 
 	} 
 
+	//Looks at each line in turn, rejects comment lines, prints data values to screen and to a txt. document, while updating minValue, maxValue etc.
 	void analyseData(String line) throws IOException { 
+		//Initialising the scanner and necessary objects for reading document and writing to a file: 
+		try(Scanner sc1 = new Scanner(line);
+				FileWriter fw = new FileWriter(dataFile1, true); //dataFile1 was initialised in analysisStart method above. The second argument, true, assures that each time we add data value to file it doesn't overwrite previous one.
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter pw = new PrintWriter(bw);
+				){
 
-		char firstCharacterOfLine = line.trim().charAt(0);//isolating the first character of string to see if line is a comment. 
-		if(line.trim().isEmpty() || Character.isLetter(firstCharacterOfLine) ){ //-ignoring blank and comment lines.
-		}  
-		//For lines containing numbers: write these numbers  
-		//one per line to the screen AND to a file; 
-		else{ 
-			try(Scanner sc1 = new Scanner(line);
-					){
+			if(line.trim().isEmpty() || Character.isLetter(line.trim().charAt(0)) ){ //-ignoring blank and comment lines.
+				//In the second If condition above^, the argument to isLetter, line.trim().charAt(0), isolates the first character of line to see if line is a comment. 
+			} 
 
-				double currentLineTotal = 0;
+			//For lines containing numbers: write these numbers 
+			//one per line to the screen AND to a file; 
+			else{ 	
+
+				double currentLineTotal = 0;//Initialising counting of data values in each line
 				int currentnValuesLine = 0;
 
 				while(sc1.hasNextDouble()){ 
 					Double token = sc1.nextDouble(); 
-					System.out.println(token); 
-					//WRITE THESE NUMBERS TO A FILE 
-					FileWriter fw = new FileWriter(dataFile1);
-					BufferedWriter bw = new BufferedWriter(fw);
-					PrintWriter pw = new PrintWriter(bw);
-					pw.println(token); 
+					System.out.println(token);//-Prints each data file read in to the screen.
+					//Write these data values to a file using PrintWriter, pw: 
+					pw.println(token);
 					//using number read in, update minValue, maxValue, nValues, sumofValues 
 					if (token < minValue){
 						minValue = token;
 					}
-					if(token >maxValue){
+					if(token > maxValue){
 						maxValue = token;
 					}
 					currentLineTotal = currentLineTotal + token;
@@ -91,34 +97,58 @@ public class NumericalReader {
 			} 
 		} 
 	} 
-	// Prints  the minimum value, the maximum value, the average value and the total number of values read. 
+
+	// Prints the minimum value, the maximum value, the average value and the total number of values read. 
 	void analysisEnd(){ 
 		System.out.println("minValue: "+minValue); 
 		System.out.println("maxValue: "+maxValue); 
 		System.out.println("nValues: "+nValues); 
-		System.out.println("sumOfValues: "+sumOfValues); 
+		System.out.println("sumOfValues: "+sumOfValues+"\n"); 
 	} 
 
-	public static void main(String[] args) throws IOException {  
+	public static void main(String[] args) throws IOException { 
+
+		//Manually enter the directory where we want resulting txt files to be stored:
+		String directory = NumericalReader.getStringFromKeyboard();
+
+		/** Document 1:**/
+
 		//Create new NumericalReader object:
 		NumericalReader nr = new NumericalReader();
-
-		BufferedReader reader = nr.brFromURL("http://foo/bar/");
+		//Takes data from online document and converts to Buffered Reader:
+		BufferedReader reader1 = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
 		//Initialise line:
 		String line = "";
 
-		//Manually enter the directory as a string
-		String directory = NumericalReader.getStringFromKeyboard();
 		//Concatenate the resulting String with the name of the specific file in which to store the data 
-		String saveFile = (directory + File.separator + filename);
-		//Pass this String saveFile thorugh analysis start
-		nr.analysisStart(saveFile); // initialize minValue etc.
-		while ((line = reader.readLine()) != null) {
+		String saveFile1 = (directory + File.separator + filename1);
+		String saveFile2 = (directory + File.separator + filename2);
+
+		//Pass this String saveFile through analysis start
+		nr.analysisStart(saveFile1); // initialise minValue etc.
+		System.out.println("Data Values from http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt:");
+
+		while ((line = reader1.readLine()) != null) {
 			nr.analyseData(line); // analyze lines, check for comments etc.
 		}
 		nr.analysisEnd(); // print min, max, etc.
 
+		/** Document 2:**/ 
+
+		//Following same procedure as above ^:
+		NumericalReader nr2 = new NumericalReader();
+		BufferedReader reader2 = nr2.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+		String line2 = "";
+
+		nr2.analysisStart(saveFile2);
+		System.out.println("Data Values from http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt:");
+
+		while ((line2 = reader2.readLine()) != null) {
+			nr2.analyseData(line2); // analyze lines, check for comments etc.
+		}
+		nr2.analysisEnd(); // print min, max, etc.
 	} 
-} 
+}
+
 
 
--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -1,11 +1,8 @@
 package module4; 
-
 //Counts the number of words in an online document 
-
 import java.io.*; 
 import java.net.*; 
 import java.util.*; 
-
 public class WordCounter { 
 	//Returns a buffered reader of an online file.
 	public static BufferedReader brFromURL(String urlName) throws IOException{ 
@@ -14,38 +11,36 @@ public class WordCounter {
 		InputStreamReader isr = new InputStreamReader(istream); 
 		BufferedReader buffr = new BufferedReader(isr); 
 		return buffr;} 
-
 	//Returns a buffered reader of a local file. 
 	public static BufferedReader brFromFile(String fileName) throws IOException{ 
 		FileReader fr = new FileReader("filename"); 
 		BufferedReader br = new BufferedReader(fr); 
 		return br; 
 	} 
-
 	public static int countWordsInResource(BufferedReader dataAsBR) throws IOException { 
 		try(Scanner scanner1 = new Scanner(dataAsBR);
-				){  
-			int wordcount = 0; 			//initialising word count 
+				){
+			int wordcount = 0; // initialising word count 
 			while (scanner1.hasNext()){ //As long as there is a word after the current position in document... 
-				wordcount++; 			//...increment the word count...  
-				scanner1.next(); 		//...and shift position to next word in document 
-			} 
-			
+				wordcount++; //...increment the word count.
+				scanner1.next(); //...and shift position to next word in document 
+			}
+
 			return wordcount;
 		}
-	} 
+	}
 
 
-	public static void main(String[] args) { 
+
+	public static void main(String[] args){ 
 		try { 
 			BufferedReader br = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"); 
 			int count = countWordsInResource(br); 
-			System.out.println("Wordcount:"+count); 
-		}  
+			System.out.println("Wordcount:"+count);
+		}
 		catch (IOException e) { 
-			e.printStackTrace(); 
-		}  
-
-	} 
+			e.printStackTrace();
+		}
+	}
 
 } 

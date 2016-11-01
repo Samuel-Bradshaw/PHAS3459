1fdd77ba267454ec1b92f0b1151e00595062b9e7
samue <samue@192.168.1.10>
2016-11-01 22:04:50 +0000

    Module 5 - SquareMatrix, toString method for representing matrices
    toString method for representing Matrices complete, representing them in
    a 2d array

--- PHAS3459/src/module5/SquareMatrix.java
+++ PHAS3459/src/module5/SquareMatrix.java
@@ -7,6 +7,7 @@ public class SquareMatrix {
 	//Member variables 
 	private double[][] matrixElements; 
 
+	//Constructor - throws exception if argument doesn't form a square Matrix
 	public SquareMatrix(double[][] elements) throws Exception {
 		for(int i = 0; i < elements.length; i++){ 
 			if ((elements[i].length == elements.length) == false){//Tests that a square matrix has been entered
@@ -15,14 +16,26 @@ public class SquareMatrix {
 		}
 		matrixElements = elements;
 	}
-
-public String toString(){
-		
-		for(int i; i < this.length; i++){
-			
+	
+	//Presents Square Matrix objects is readable form
+	public String toString(){
+		String matstring = "(";
+		for(int row = 0; row < matrixElements.length; row++){
+			for(int column = 0; column < matrixElements[row].length; column++){
+				if(column < (matrixElements.length - 1)){
+					matstring +=  matrixElements[row][column] +"\t";
+				}
+				else{ //if the element is the last one in the row, don't add the space "\t"
+					matstring +=  matrixElements[row][column];
+				}
+			}
+			if(row < (matrixElements.length - 1)){
+				matstring +=  "\n ";
+			}
+			else{ //if the element is the last one in the column 
+				matstring += ")";}
 		}
-		
-			
-		//}
-//	}
+		return matstring;
+	}
+
 }
--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -2,11 +2,13 @@ package module5;
 
 public class TestSquareMatrix {
 
-	static double[][] sq1 = {{1,2},{3,4}}; 
+	static double[][] sq1 = {{1,2,3},{3,4,5},{7,8,9}}; 
 	
 	public static void main(String[] args) {
 		try{SquareMatrix sm1 = new SquareMatrix(sq1);
-		System.out.println(sm1);}
+		System.out.println(sm1);
+		System.out.println(sq1.length);
+		}
 		catch(Exception e){
 			System.out.println(e);
 		}
